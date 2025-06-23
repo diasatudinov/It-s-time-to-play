22287272083dcda9221f0fa5dbec17543cc5bea5
Dias <dias.atudinov@gmail.com>
2025-06-23 22:34:40 +0500

    add menu and splash screen view

--- Its time to play/Its time to play/ITTPViews/ITTPMenuView/ITTPMenuView.swift
+++ Its time to play/Its time to play/ITTPViews/ITTPMenuView/ITTPMenuView.swift
@@ -2,15 +2,94 @@
 //  ITTPMenuView.swift
 //  Its time to play
 //
-//  Created by Dias Atudinov on 23.06.2025.
 //
 
 import SwiftUI
 
 struct ITTPMenuView: View {
+    @State private var showGame = false
+    @State private var showAchievement = false
+    @State private var showSettings = false
+    
+    
+    @StateObject var achievementVM = SRAchievementsViewModel()
+    @StateObject var settingsVM = SettingsViewModelSR()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            VStack(spacing: 0) {
+                HStack(alignment: .top) {
+                    Button {
+                        showAchievement = true
+                    } label: {
+                        ZStack {
+                            Image(.achievementsIconITTP)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SRDeviceInfo.shared.deviceType == .pad ? 140:72)
+                            
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        showSettings = true
+                    } label: {
+                        Image(.settingsIconITTP)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: SRDeviceInfo.shared.deviceType == .pad ? 140:72)
+                    }
+                }
+                
+                Spacer()
+                
+                Button {
+                    showGame = true
+                    
+                } label: {
+                    
+                    ZStack {
+                        Image(.newGameIconITTP)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: SRDeviceInfo.shared.deviceType == .pad ? 210:150)
+                        
+                        
+                        
+                    }
+                }
+                
+                
+            }
+            
+            
+        }.padding()
+            .background(
+                ZStack {
+                    Image(.appBgITTP)
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .scaledToFill()
+                }
+            )
+            .fullScreenCover(isPresented: $showGame) {
+                //            SRGameLevelsView()
+            }
+            .fullScreenCover(isPresented: $showAchievement) {
+                ITTPAchievementsView(viewModel: achievementVM)
+            }
+            .fullScreenCover(isPresented: $showSettings) {
+                ITTPSettingsView(settingsVM: settingsVM)
+            }
+        
+        
+        
+        
     }
+    
 }
 
 #Preview {
--- Its time to play/Its time to play/ITTPViews/ITTPMenuView/ITTPSplashScreen.swift
+++ Its time to play/Its time to play/ITTPViews/ITTPMenuView/ITTPSplashScreen.swift
@@ -2,14 +2,50 @@
 //  ITTPSplashScreen.swift
 //  Its time to play
 //
-//  Created by Dias Atudinov on 23.06.2025.
 //
 
 import SwiftUI
 
 struct ITTPSplashScreen: View {
+    @State private var scale: CGFloat = 1.0
+    @State private var progress: CGFloat = 0.0
+    @State private var timer: Timer?
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(.loaderBgITTP)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                
+                Spacer()
+                
+                
+                Text("Loading (\(Int(progress))%)")
+                    .font(.custom(Fonts.regular.rawValue, size: 44))
+                    .foregroundStyle(.white)
+                    .padding(.bottom, 32)
+                
+            }
+            
+            
+        }
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 100 {
+                progress += 1
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
 
