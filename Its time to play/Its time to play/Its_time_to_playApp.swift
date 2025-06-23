133805e4ef4ceb6be85551faae34b7c89bc4afb7
Dias <dias.atudinov@gmail.com>
2025-06-23 21:38:19 +0500

    app setup

---
+++ Its time to play/Its time to play/Its_time_to_playApp.swift
@@ -0,0 +1,17 @@
+//
+//  Its_time_to_playApp.swift
+//  Its time to play
+//
+//  Created by Dias Atudinov on 23.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Its_time_to_playApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
