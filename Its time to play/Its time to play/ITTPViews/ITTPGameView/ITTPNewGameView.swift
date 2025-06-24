e5637c4e8168f1dbf7ac4b247b3ff3d2b54d9dad
Dias <dias.atudinov@gmail.com>
2025-06-24 18:18:18 +0500

    new files

---
+++ Its time to play/Its time to play/ITTPViews/ITTPGameView/ITTPNewGameView.swift
@@ -0,0 +1,18 @@
+//
+//  ITTPNewGameView.swift
+//  Its time to play
+//
+//  Created by Dias Atudinov on 24.06.2025.
+//
+
+import SwiftUI
+
+struct ITTPNewGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ITTPNewGameView()
+}
---
+++ Its time to play/Its time to play/ITTPViews/ITTPGameView/ITTPNewGameViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ITTPNewGameViewModel.swift
+//  Its time to play
+//
+//  Created by Dias Atudinov on 24.06.2025.
+//
+
+import Foundation
