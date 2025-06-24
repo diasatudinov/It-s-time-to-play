5a59cb32b72351293ab5a6d94da9b78017257bc0
Dias <dias.atudinov@gmail.com>
2025-06-25 00:03:51 +0500

    new files

---
+++ Its time to play/Its time to play/ITTPViews/ITTPGameView/ITTPLevelView.swift
@@ -0,0 +1,18 @@
+//
+//  ITTPLevelView.swift
+//  Its time to play
+//
+//  Created by Dias Atudinov on 24.06.2025.
+//
+
+import SwiftUI
+
+struct ITTPLevelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ITTPLevelView()
+}
